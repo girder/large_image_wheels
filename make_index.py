aa8b477fa018999cf3669e64bdd35df60bce792f
David Manthey <david.manthey@kitware.com>
2018-11-13 09:50:24 -0500

    Undo redirection.

--- make_index.py
+++ make_index.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 
-import json
 import os
-import requests
-import sys
 
 path = 'docs'
 indexName = 'index.html'
@@ -18,19 +15,7 @@ template = """<html>
 """
 link = '<a href="%s" download="%s">%s</a>'
 
-if sys.argv[1:] == ['--data']:
-    GirderUrl = 'https://data.kitware.com/api/v1'
-    items = requests.get(GirderUrl + '/resource/search', params={
-        'mode': 'text',
-        'types': json.dumps(['item']),
-        'q': '"Large Image Wheels 2018-11-13"'
-    }).json()['item']
-    itemId = items[0]['_id']
-    files = requests.get(GirderUrl + '/item/%s/files' % itemId).json()
-    wheels = [(file['name'], GirderUrl + '/file/%s/download' % file['_id'])
-              for file in files if 'whl' in file['exts']]
-else:
-    wheels = [(name, name) for name in os.listdir(path) if name.endswith('whl')]
+wheels = [(name, name) for name in os.listdir(path) if name.endswith('whl')]
 
 wheels = sorted(wheels)
 index = template.replace('%LINKS%', '\n'.join([
